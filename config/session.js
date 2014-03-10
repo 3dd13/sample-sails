15c55eab148f99c98d8068013fb6d7536b5e4634
Eddie Lau <hi@3dd13.me>
2014-03-10 19:28:36 +0800

    fix comma

--- config/session.js
+++ config/session.js
@@ -15,7 +15,7 @@ module.exports.session = {
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
   // forcing them to log in again.
-  secret: '5f65bbd7f33150b86e5bd9c433532f10'
+  secret: '5f65bbd7f33150b86e5bd9c433532f10',
 
 
   // In production, uncomment the following lines to set up a shared redis session store
